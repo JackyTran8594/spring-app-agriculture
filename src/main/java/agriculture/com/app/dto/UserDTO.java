41c207189a1a8369f58069f9754277c19c1153b9
jackytran <bachtuvu.dev@gmail.com>
2020-12-16 18:12:15 +0700

    add flyway

---
+++ src/main/java/agriculture/com/app/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package agriculture.com.app.dto;
+
+public class UserDTO {
+    public long id;
+    public String firstName;
+    public String lastName;
+    public String email;
+
+    // public UserDTO(long id, String firstName, String lastName, String email) {
+    // this.id = id;
+    // this.firstName = firstName;
+    // this.lastName = lastName;
+    // this.email = email;
+    // };
+}
