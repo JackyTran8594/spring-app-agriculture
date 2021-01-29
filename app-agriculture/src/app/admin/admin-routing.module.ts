b6f2e118410a5fad1e8019a6fa5f8aabe0a48fc9
jackytran <bachtuvu.dev@gmail.com>
2021-01-29 17:41:18 +0700

    29012021

--- app-agriculture/src/app/admin/admin-routing.module.ts
+++ app-agriculture/src/app/admin/admin-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-  // { path: '', component: AdminComponent, pathMatch: 'full' },
+  { path: '', component: AdminComponent, pathMatch: 'full' },
   {path:'admin/post', loadChildren: () => import('./post/post.module').then(m=>m.PostModule)}
  
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    
+    // RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 
